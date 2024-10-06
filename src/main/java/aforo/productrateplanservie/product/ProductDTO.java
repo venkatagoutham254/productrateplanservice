d5b8c6564853f90b6e2cfb81ac7f7bf03a7004e7
buchwa2 <beswar@aforo.ai>
2024-10-06 12:44:43 +0200

    Update ProductDTO.java
    changes to the DTO
    
    Co-Authored-By: muralidevarapalli <183363855+muralidevarapalli@users.noreply.github.com>

--- src/main/java/aforo/productrateplanservie/product/ProductDTO.java
+++ src/main/java/aforo/productrateplanservie/product/ProductDTO.java
@@ -22,4 +22,46 @@ public class ProductDTO {
     @Size(max = 255)
     private String status;
 
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public ProductDTO(Long productId, @NotNull @Size(max = 100) String productName, String productDescription,
+			@NotNull @Size(max = 255) String status) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productDescription = productDescription;
+		this.status = status;
+	}
+    
+
 }
